9a2130a113ab825e16d03b868e4aab4e1ab316e8
atom3219 <96765319+atom3219@users.noreply.github.com>
2022-03-11 11:00:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
-    } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
-    }
+<!--
+function play() {
+	document.rabin.play()
 }
+function stop() {
+	document.rabin.stop()
+}
+function rewind() {
+	document.rabin.rewind()
+}
+function exec(func) {
+	func()
+}
+// -->
+</SCRIPT>
+
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+with (document) {
+	write('<TABLE BORDER=2 CELLPADDING=7><TR><TD ALIGN="center">')
+	write('<EMBED SRC="rabin.avi" AUTOSTART=FALSE LOOP=FALSE HEIGHT=120 WIDTH=159 NAME="rabin">')
+	write('</TD></TR><TR><TD ALIGN="center"><FORM>')
+	write('<INPUT TYPE="button" VALUE="play" onClick="exec(play)">')
+	write('<INPUT TYPE="button" VALUE="stop" onClick="exec(stop)">')
+	write('<INPUT TYPE="button" VALUE="rewind" onClick="exec(rewind)">')
+	write('</TD></TR></TABLE></FORM>')
+}
+
+// -->
+</SCRIPT>
